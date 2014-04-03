5fffb5f3d0f05e32cfa209899becc00f8a395c2b
i.khrulev <I.Khrulev@qiwi.ru>
2014-04-03 17:08:09 +0400

    Full-page map by default

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -1,5 +1,9 @@
 define('map', ['ymaps', 'search'], function(ymaps, search) {
 	ymaps.ready(function() {
 		search.query('test');
+		new ymaps.Map('map', {
+			center:[55.637065, 37.598555],
+			zoom: 16
+		});
 	});
 });
