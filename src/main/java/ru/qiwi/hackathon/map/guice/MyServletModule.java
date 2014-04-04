b60245ba66ab9a2e79d56db150baf72d05770984
d.boldyrev <D.Boldyrev@qiwi.ru>
2014-04-04 12:40:30 +0400

    api prefix

--- src/main/java/ru/qiwi/hackathon/map/guice/MyServletModule.java
+++ src/main/java/ru/qiwi/hackathon/map/guice/MyServletModule.java
@@ -9,8 +9,8 @@ public class MyServletModule extends ServletModule {
 
 	@Override
 	protected void configureServlets() {
-		serve("/load_points").with(LoadPointsProcessor.class);
-		serve("/load_object").with(LoadObjectProcessor.class);
-		serve("/search").with(SearchProcessor.class);
+		serve("/api/load_points").with(LoadPointsProcessor.class);
+		serve("/api/load_object").with(LoadObjectProcessor.class);
+		serve("/api/search").with(SearchProcessor.class);
 	}
 }
